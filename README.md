14030007ddc19f0be5b0620d1a285dc9edca6e04
salnav <sal.navroz@gmail.com>
2024-05-07 00:08:52 -0700

    testing branch: should be wrong

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Lab 5 - Starter
 Name: Salman Navroz
+
+1. You can technically unit test the messaging feature by checking if the other user received the message successfully, but it would be more ideal to test each subcomponent of this process with unit tests. This means testing if the writing works, sending works, receiving works, as well as all the other processes. This ensures that the entire messaging part of the application works because you verified that each smaller process works already with unit tests.
+2. Yes it is good to unit test the "max message length" because this is an edge case that is easier to debug on a small scale. This is easy to unit test with a hard stop at 80 characters, so it is good to ensure that it will not be a problem in the real production app.
--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -4,4 +4,37 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  const voiceSelect = document.getElementById('voice-select');
+  const talkButton = document.querySelector('button');
+  const textInput = document.getElementById('text-to-speak');
+  const faceImage = document.querySelector('img');
+
+  function populateVoiceList() {
+    const voices = window.speechSynthesis.getVoices(); 
+    voices.forEach((voice) => {
+      const option = document.createElement('option');
+      option.value = voice.name;
+      option.textContent = `${voice.name} (${voice.lang})`;
+      voiceSelect.appendChild(option);
+    });
+  }
+
+  populateVoiceList();
+  if (window.speechSynthesis.onvoiceschanged !== undefined) {
+    window.speechSynthesis.onvoiceschanged = populateVoiceList;
+  }
+
+  talkButton.addEventListener('click', () => {
+    const utterance = new SpeechSynthesisUtterance(textInput.value);
+    const selectedVoice = voiceSelect.selectedOptions[0].value;
+    const voices = window.speechSynthesis.getVoices();
+    utterance.voice = voices.find((voice) => voice.name === selectedVoice);
+    utterance.onstart = function () {
+      faceImage.src = 'assets/images/smiling-open.png';
+    };
+    utterance.onend = function () {
+      faceImage.src = 'assets/images/smiling.png';
+    };
+    window.speechSynthesis.speak(utterance);
+  });
 }
